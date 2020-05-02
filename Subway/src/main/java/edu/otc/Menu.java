43efa9fd4218e5a43d954a268feccbc77094651d
Sforren <57277165+Sforren@users.noreply.github.com>
2020-05-01 20:09:21 -0700

    Subway 1.0

--- Subway/src/main/java/edu/otc/Menu.java
+++ Subway/src/main/java/edu/otc/Menu.java
@@ -5,13 +5,175 @@
  */
 package edu.otc;
 
+import static edu.otc.Subway.breadList;
+import static edu.otc.Subway.size;
+import java.util.Scanner;
+
 /**
  *
  * @author Dima
  */
 public class Menu { // this class should display the menu with availible items and update the menu as the customer selects more items.
-    public int steps; // count the step that the customer is on and display the approporiate menu options. if they are on the final step, display the price and the thank you message
-    public static void main(String[] args){
-        
+
+    DisplayOrder display = new DisplayOrder();
+    public int steps; // count the step that the customer is on and display the approporiate menu options. if they are on the final step,
+    //display the price and the thank you message. this is for the javafx.
+
+    public void selectSize() {
+        System.out.println("Welcome to Subway.");
+        System.out.println("Would you like a 6-inch or a 12-inch sub?");
+        System.out.println("type 1 for 6 inch, type 2 for 12 inch");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 2) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for 6 inch, type 2 for 12 inch");
+            choice = input.nextInt();
+            if (choice == 1) {
+                size = 6;
+            } else if (choice == 2) {
+                size = 12;
+            }
+        }
+    }
+
+    public void selectBread() {
+        System.out.println("What kind of bread would you like?");
+        System.out.println("type 1 for White, 2 for Italian Herbs and Cheese, 3 for Roasted Garlic, 4 for 9 Grain Wheat");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 4) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for White, 2 for Italian Herbs and Cheese, 3 for Roasted Garlic, 4 for 9 Grain Wheat");
+            choice = input.nextInt();
+
+            display.currentOrder[1] = breadList[choice - 1];
+        }
+
+    }
+
+    public void selectMeat() {
+        System.out.println("What kind of meat would you like?");
+        System.out.println("type 1 for ham, 2 for tuna, 3 for Turkey, 4 for Teriyaki Chicken");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 4) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for ham, 2 for tuna, 3 for Turkey, 4 for Teriyaki Chicken");
+            choice = input.nextInt();
+
+            display.currentOrder[2] = breadList[choice - 1];
+        }
+
+    }
+
+    public void selectCheese() {
+        System.out.println("What kind of cheese would you like?");
+        System.out.println("type 1 for American, 2 for Cheddar, 3 for Provalone, 4 for Swiss");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 4) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for American, 2 for Cheddar, 3 for Provalone, 4 for Swiss");
+            choice = input.nextInt();
+
+            display.currentOrder[3] = breadList[choice - 1];
+        }
+
+    }
+    
+    /*
+    public void selectVeggies() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("What kind of veggies would you like? pick up to 3");
+            System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+            Scanner input = new Scanner(System.in);
+            int choice = input.nextInt();
+            while (choice < 1 || choice > 5) {
+                System.out.println("Invalid Selection");
+                System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+                choice = input.nextInt();
+
+                display.currentOrder[4 + i] = breadList[choice - 1];
+            }
+        }
+    }
+    */
+    
+    public void selectVeggies1() {
+
+        System.out.println("What kind of veggies would you like? pick up to 3");
+        System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 5) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+            choice = input.nextInt();
+
+            display.currentOrder[4] = breadList[choice - 1];
+        }
+
+    }
+
+    public void selectVeggies2() {
+
+        System.out.println("What kind of veggies would you like? pick up to 3");
+        System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 5) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+            choice = input.nextInt();
+
+            display.currentOrder[5] = breadList[choice - 1];
+        }
+
+    }
+
+    public void selectVeggies3() {
+
+        System.out.println("What kind of veggies would you like? pick up to 3");
+        System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 5) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for onions, 2 for lettuce, 3 for tomatoes, 4 for pickles, 5 for nothing");
+            choice = input.nextInt();
+
+            display.currentOrder[6] = breadList[choice - 1];
+        }
+
+    }
+
+    public void selectSauce() {
+        System.out.println("What kind of sauce would you like?");
+        System.out.println("type 1 for mayonnaise, 2 for ketchup, 3 for mustard, 4 ranch");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 4) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for mayonnaise, 2 for ketchup, 3 for mustard, 4 ranch");
+            choice = input.nextInt();
+
+            display.currentOrder[7] = breadList[choice - 1];
+        }
+
+    }
+
+    public void selectSpices() {
+        System.out.println("Would you like salt or pepper?");
+        System.out.println("type 1 for salt, 2 for pepper, 3 for both");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        while (choice < 1 || choice > 4) {
+            System.out.println("Invalid Selection");
+            System.out.println("type 1 for salt, 2 for pepper, 3 for both");
+            choice = input.nextInt();
+
+            display.currentOrder[8] = breadList[choice - 1];
+        }
     }
 }
--- Subway/src/main/java/edu/otc/Subway.java
+++ Subway/src/main/java/edu/otc/Subway.java
@@ -11,15 +11,28 @@ package edu.otc;
  */
 public class Subway { // this is the main class. it stores ingredients and has methods to add and remove them.
 
-    public static void main(String[] args) {
-        // todos
-        // add more ingredients
-        // 
-        // create gui 
-    }
-    public int size;
+    public static int size;
     public double price = 0;
     public double finalPrice = 0;
+
+    public static void main(String[] args) {
+       Menu menu = new Menu();
+       menu.selectSize();
+       menu.selectBread();
+       menu.selectMeat();
+       menu.selectCheese();
+       menu.selectVeggies1(); // for whatever reason the for loop didnt want to work so i had to do this ugly workaround
+       menu.selectVeggies2();
+       menu.selectVeggies3();
+       menu.selectSauce();
+       menu.selectSpices();
+               
+        // todos
+        // display the questions in a gui
+        // display the currentOrder[] array in a gui
+        // i added some prices to the ingredients but we probably dont have time for this anymore
+        // add images (we probably dont have time for this anymore either)
+    }
 
     public double finalPrice() {
 
@@ -39,11 +52,10 @@ public class Subway { // this is the main class. it stores ingredients and has m
 
     public static final Ingredient[] meatList = {
         new Ingredient("Ham", 2.00, "ham.png"),
-        new Ingredient("Black Forest Ham", 2.00, "blackForest.png"),
         new Ingredient("Tuna", 2.00, "tuna.png"),
         new Ingredient("Turkey", 2.00, "turkey.png"),
         new Ingredient("Teriyaki Chicken", 2.00, "teriyakiChicken.png")
-            
+
     };
 
     public static final Ingredient[] cheeseList = {
@@ -51,24 +63,28 @@ public class Subway { // this is the main class. it stores ingredients and has m
         new Ingredient("Cheddar", 2.00, "cheddarCheese.png"),
         new Ingredient("Provalone", 2.00, "provaloneCheese.png"),
         new Ingredient("Swiss", 2.00, "swissCheese.png")
-           
+
     };
 
     public static final Ingredient[] veggiesList = {
         new Ingredient("Onions", 0.50, "onions.png"),
         new Ingredient("Lettuce", 0.50, "lettuce.png"),
         new Ingredient("Tomato", 0.50, "tomato.png"),
-        new Ingredient("Peppers", 0.50, "peppers.png"),
         new Ingredient("Pickles", 0.50, "pickles.png"),
-        new Ingredient("Cucumbers", 0.50, "cucumbers.png")
+        new Ingredient("Nothing", 0.00, "nothing.png")
     };
 
     public static final Ingredient[] SauceList = {
         new Ingredient("Mayonnaise", 0.25, "mayonnaise.png"),
         new Ingredient("Ketchup", 0.25, "ketchup.png"),
         new Ingredient("Mustard", 0.25, "mustard.png"),
-        new Ingredient("Oil", 0.25, "oil.png"),
         new Ingredient("Ranch", 0.25, "ranch.png")
+    };
+    
+    public static final Ingredient[] SpiceList = {
+        new Ingredient("Salt", 0.00, "salt.png"),
+        new Ingredient("Pepper", 0.00, "pepper.png"),
+        new Ingredient("Salt and Pepper", 0.00, "saltAndPepper.png")
     };
 
 }
